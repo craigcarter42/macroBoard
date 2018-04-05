4d5112593660e7696fab87b5e4fddf147eeb3fdb
Craig Carter <32251992+craigcarter42@users.noreply.github.com>
2018-04-04 21:28:20 -0700

    Update macroBoard.ino

--- macroBoard.ino
+++ macroBoard.ino
@@ -1,59 +1,48 @@
 #include "Keyboard.h"
 
 void setup() {
-  pinMode(6, INPUT_PULLUP);
-  pinMode(8, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+  pinMode(16, INPUT_PULLUP);
   pinMode(9, INPUT_PULLUP);
-  pinMode(10, INPUT_PULLUP);
+  pinMode(15, INPUT_PULLUP);
 
   Keyboard.begin();
 }
 
 void loop()
 {
-  if(digitalRead(6) == LOW)
+  if(digitalRead(14) == LOW)
   {
-    // new window
-    Keyboard.press(0x83);
-    Keyboard.press('n');
-    delay(500);
-    Keyboard.releaseAll();
-  }
-  if(digitalRead(8) == LOW)
-  {
-    // type password
-    Keyboard.println("password");
-    delay(500);
-  }
-  if(digitalRead(9) == LOW)
-  {
-    // copy
+    // cmd copy
     Keyboard.press(0x83);
     Keyboard.press('c');
     delay(500);
     Keyboard.releaseAll();
   }
-  if(digitalRead(10) == LOW)
+  if(digitalRead(15) == LOW)
   {
-    // paste
+    // cmd paste
     Keyboard.press(0x83);
     Keyboard.press('v');
     delay(500);
     Keyboard.releaseAll();
   }
-  if(digitalRead(2) == LOW)
+  if(digitalRead(16) == LOW)
   {
-    // cut
     Keyboard.press(0x83);
     Keyboard.press('x');
     delay(500);
     Keyboard.releaseAll();
   }
-   if(digitalRead(3) == LOW)
+  if(digitalRead(9) == LOW)
   {
-    // paste
-    Keyboard.press(0x83);
-    Keyboard.press('w');
+// select account and enter password
+    Keyboard.press('c');
+    Keyboard.press(0xB0);
+    Keyboard.releaseAll();
+    delay(500);
+    Keyboard.println("password");
+    Keyboard.press(0xB0);
     delay(500);
     Keyboard.releaseAll();
   }
